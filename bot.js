79aab91c07db366b4f867cd33c0b27b8ab41716a
Bakedpork <40089827+Bakedpork@users.noreply.github.com>
2018-06-10 10:07:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,36 @@ client.on('message', message => {
     	message.reply('HAHA');
   	}
 });
+client.on('message', message => {
+    if (message.content === 'R6') {
+    	message.reply('寂寞我們來Rainbow:SIECE');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Minecraft') {
+    	message.reply('寂寞我們來Hypixel');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'PUBG') {
+    	message.reply('寂寞我們來PUBG');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'OW') {
+    	message.reply('寂寞我們來overwatch');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'YT) {
+    	message.reply('https://www.youtube.com/channel/UCLlcPDPQ4wo9WhUL2CR2RWw?view_as=subscriber');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'LonelyYT) {
+    	message.reply('https://www.youtube.com/channel/UCiL9eRN8QbWCOuw8HD_LOpA');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
